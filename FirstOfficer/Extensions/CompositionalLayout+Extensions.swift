e1afaf9243a8a90443c9572961019656073a5587
Deniz Can Beytaş <70637660+denizcanbeytas@users.noreply.github.com>
2022-08-30 04:01:15 +0300

    Compositional Layout Added

---
+++ FirstOfficer/Extensions/CompositionalLayout+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  CompositionalLayout+Extensions.swift
+//  FirstOfficer
+//
+//  Created by Deniz on 30.08.2022.
+//
+
+import Foundation
+import UIKit
+
+enum CompositionalGroupAlignment {
+    case vertical
+    case horizontal
+}
+
+struct CompositionalLayout {
+    
+    static func createItem(width: NSCollectionLayoutDimension, height: NSCollectionLayoutDimension, spacing: CGFloat) -> NSCollectionLayoutItem {
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: width, heightDimension: height))
+        item.contentInsets = NSDirectionalEdgeInsets(top: spacing, leading: spacing, bottom: spacing, trailing: spacing)
+        return item
+    }
+    
+    static func createGroup(alignment: CompositionalGroupAlignment, width: NSCollectionLayoutDimension, height: NSCollectionLayoutDimension, items: [NSCollectionLayoutItem]) -> NSCollectionLayoutGroup {
+        switch alignment {
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width,
+                                                                                heightDimension: height),
+                                                                                subitems: items)
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width,
+                                                                                    heightDimension: height),
+                                                                                    subitems: items)
+        }
+    }
+    
+    static func createGroup(alignment: CompositionalGroupAlignment, width: NSCollectionLayoutDimension, height: NSCollectionLayoutDimension, item: NSCollectionLayoutItem, count: Int) -> NSCollectionLayoutGroup {
+        switch alignment {
+        case .vertical:
+            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width,
+                                                                                  heightDimension: height),
+                                                                                  subitem: item,
+                                                                                  count: count)
+        case .horizontal:
+            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width,
+                                                                                    heightDimension: height),
+                                                                                    subitem: item,
+                                                                                    count: count)
+        }
+    }
+}
--- FirstOfficer/Scenes/DetailImagesFullScreen/FullScreenViewController.swift
+++ FirstOfficer/Scenes/DetailImagesFullScreen/FullScreenViewController.swift
@@ -23,7 +23,8 @@ final class FullScreenViewController: UIViewController {
     var viewModel: FullScreen.Fetch.ViewModel?
     weak var delegateFullScreen: FullScreenDelegate?
     
-  //  @IBOutlet weak var imageView: UIImageView!
+    var isTapped = true
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: Object lifecycle
@@ -37,22 +38,30 @@ final class FullScreenViewController: UIViewController {
         super.init(coder: aDecoder)
         setup()
     }
+    
     override func viewDidLoad() {
         interactor?.fetchImage(request: FullScreen.Fetch.Request())
         collectionView.register(UINib(nibName: "FullScreenCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "FullScreenCollectionViewCell")
-        //imageView.sd_setImage(with: URL(string: viewModel?.image ?? "")) -> image içindi artık kullanılmıyor
-        
+        setNavigationBarRightItem(image: "setGridImage")
     }
     
-    // MARK: for image indexPath
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.navigationBar.tintColor = .white
+   }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+ 
+    //for image indexPath
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         collectionView.scrollToItem(at: IndexPath(row: viewModel?.selectedIndex ?? 0, section: 0), at: .left, animated: true)
     }
     
-    
     // MARK: Setup
-    
     private func setup() {
         let viewController = self
         let interactor = FullScreenInteractor()
@@ -64,6 +73,21 @@ final class FullScreenViewController: UIViewController {
         presenter.viewController = viewController
         router.viewController = viewController
         router.dataStore = interactor
+    }
+
+    @objc func changeGrid() {
+        if isTapped {
+            setNavigationBarRightItem(image: "setGridImageSecond")
+            collectionView.setCollectionViewLayout(createLayout(), animated: true)
+            isTapped = false
+            print(isTapped)
+        }else {
+            setNavigationBarRightItem(image: "setGridImage")
+            //collectionView.collectionViewLayout = setCollectionView()
+            collectionView.setCollectionViewLayout(setCollectionView(), animated: true)
+            isTapped = true
+            print(isTapped)
+        }
     }
 }
 
@@ -86,10 +110,9 @@ extension FullScreenViewController: UICollectionViewDelegate, UICollectionViewDa
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FullScreenCollectionViewCell", for: indexPath) as? FullScreenCollectionViewCell
         guard let model = self.viewModel else {return UICollectionViewCell()}
-       // cell?.configCell(viewModel: model)
+        
         cell?.configCell(images: model.images?[indexPath.row] ?? "")
-       // cell?.configureCell(image: model.images?[indexPath.row] ?? "")
-        collectionView.layoutIfNeeded()
+        
         return cell ?? UICollectionViewCell()
     }
     
@@ -105,6 +128,37 @@ extension FullScreenViewController: UICollectionViewDelegate, UICollectionViewDa
             delegateFullScreen?.fullScreenDidScroll(indexPath: visibleIndexPath)
         }
     }
+}
+
+extension FullScreenViewController {
     
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        let item = CompositionalLayout.createItem(width: .fractionalWidth(0.5), height: .fractionalHeight(1), spacing: 1)
+        
+        let fullItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(1), spacing: 1)
+        let verticalGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(0.5), height: .fractionalHeight(1), item: fullItem, count: 2)
+        
+        let horizontalGroup = CompositionalLayout.createGroup(alignment: .horizontal, width: .fractionalWidth(1), height: .fractionalHeight(0.6), items: [item, verticalGroup])
+        
+        let mainItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(0.4), spacing: 1)
+        let mainGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(1), height: .fractionalHeight(0.5), items: [mainItem, horizontalGroup])
+        
+        let section = NSCollectionLayoutSection(group: mainGroup)
+        
+        // return
+        return UICollectionViewCompositionalLayout(section: section)
+    }
     
+    private func setCollectionView() -> UICollectionViewFlowLayout {
+        let horizontalList = UICollectionViewFlowLayout()
+        horizontalList.scrollDirection = .horizontal
+        horizontalList.itemSize = CGSize(width: (view.frame.size.width - 40), height: (collectionView.frame.size.width + 100))
+        return horizontalList
+    }
+    
+    private func setNavigationBarRightItem(image: String) {
+        let changeButton = UIBarButtonItem.init(image: UIImage(named: image), style: .done, target: self, action: #selector(changeGrid))
+        navigationItem.rightBarButtonItems = [changeButton]
+        
+    }
 }
